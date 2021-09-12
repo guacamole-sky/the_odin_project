9f9390bf22b5bab98b12aecc1159a46136f60cc3
guacamole-sky <guacamole-sky@protonmail.com>
2021-09-12 12:00:20 +0700

    test

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,16 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'odin-recipes/index.html')
+        odin_recipes: resolve(__dirname, 'odin_recipes/index.html')
       }
     }
   },
-  base: ""
+  base: "/the_odin_project/"
 })
 
